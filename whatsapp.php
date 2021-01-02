f148a3bad366876f9767eb5f041c5bf556e52ec2
JoaoPauloMP0105 <joaopaulokbca@hotmail.com>
2021-01-02 12:45:27 -0300

    alterando a chamada da api

--- whatsapp.php
+++ whatsapp.php
@@ -8,14 +8,14 @@ $ipod = strpos($_SERVER['HTTP_USER_AGENT'],"iPod");
 // check if is a mobile
 if ($iphone || $android || $palmpre || $ipod || $berry == true)
 {
-  header('Location: https://api.whatsapp.com/send?phone=5511971456442&text=Olá, deixe sua mensagem em breve retornaremos o contato');
+  header('Location: https://api.whatsapp.com/send?phone=5511971456442&text=Olá%20Preciso%20de%20um%20Orçamento!');
   //OR
-  echo "<script>window.location='https://api.whatsapp.com/send?phone=5511971456442&text=Olá, deixe sua mensagem em breve retornaremos o contato'</script>";
+  echo "<script>window.location='https://api.whatsapp.com/send?phone=5511971456442&text=Olá%20Preciso%20de%20um%20Orçamento!'</script>";
 }
 // all others
 else {
-  header('Location: https://web.whatsapp.com/send?phone=5511971456442&text=Olá, deixe sua mensagem em breve retornaremos o contato');
+  header('Location: https://web.whatsapp.com/send?phone=5511971456442&text=Olá%20Preciso%20de%20um%20Orçamento!');
   //OR
-  echo "<script>window.location='https://web.whatsapp.com/send?phone=5511971456442&text=Olá, deixe sua mensagem em breve retornaremos o contato'</script>";
+  echo "<script>window.location='https://web.whatsapp.com/send?phone=5511971456442&text=Olá%20Preciso%20de%20um%20Orçamento!'</script>";
 }
 ?>
